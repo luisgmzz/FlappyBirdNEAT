4bc2ac3dfc41440005a4515bfa22fa926d08f77c
luisjg04 <luisjg04@ucm.es>
2025-04-25 23:56:58 +0200

    añadida pipe

---
+++ pipe.py
@@ -0,0 +1,49 @@
+from utils import load_image
+from bird import Bird
+import pygame
+import random
+
+class Pipe:
+    IMG = load_image("pipe.png")
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(self.IMG, False, True)
+        self.PIPE_BOTTOM = self.IMG
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height+self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win: pygame.Surface):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird: Bird):
+        bird_mask = bird.get_mask()
+
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        return (t_point or b_point)
+
+
