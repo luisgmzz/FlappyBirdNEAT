d5da3889db20a151f5146e17fe7640eab77a170e
luisjg04 <luisjg04@ucm.es>
2025-04-26 00:06:02 +0200

    arreglado bug a la hora de pintar la base

--- base.py
+++ base.py
@@ -15,7 +15,7 @@ class Base:
         self.x1 -= self.VEL
         self.x2 -= self.VEL
 
-        if self.x1 + self.width < 0:
+        if self.x1 + self.WIDTH < 0:
             self.x1 = self.x2 + self.WIDTH
 
         if self.x2 + self.WIDTH < 0:
@@ -23,6 +23,6 @@ class Base:
 
     def draw(self, win: pygame.Surface):
         win.blit(self.IMG, (self.x1, self.y))
-        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
 
     
